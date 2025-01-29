3988a058d0213f480ded0ea6e5d72b9df562a035
Eranda Bandaranaike <ergroove@live.com>
2025-01-29 07:47:41 +0530

    Age will update according to the birthday

--- app/Console/Commands/UpdatePatientAges.php
+++ app/Console/Commands/UpdatePatientAges.php
@@ -25,30 +25,29 @@ class UpdatePatientAges extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle(): void
     {
-        $patients = Patient::select('name', 'id', 'age', 'birthday')->get();
+        $patients = Patient::select('name', 'id', 'age', 'birthday', 'created_at')->get();
+        $updatedCount = 0;
         foreach ($patients as $patient) {
 
             if ($patient->birthday) {
-                $this->info("Found birthday for " . $patient->name . ': ' . $patient->birthday);
                 $age = Carbon::parse($patient->birthday)->age;
             } else {
                 $createdAt = Carbon::parse($patient->created_at);
                 $currentDate = Carbon::now();
                 if ($currentDate->month === $createdAt->month && $currentDate->day === $createdAt->day) {
-                    $this->info("Age can update for " . $patient->name);
                     $age = $patient->age + 1;
                 } else {
-                    $this->info("Age will not change for " . $patient->name);
                     $age = $patient->age;
                 }
             }
             if ($patient->age != $age) {
                 $patient->age = $age;
                 $patient->save();
-                $this->info("Age has been update for " . $patient->name);
+                $updatedCount++;
             }
         }
+        $this->info("Updated $updatedCount patient ages");
     }
 }
