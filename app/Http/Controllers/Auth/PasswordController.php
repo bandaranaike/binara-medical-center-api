c990d6c9fb7fd06fc53888a21efa55de3bbeb7dc
Eranda Bandaranaike <ergroove@live.com>
2025-02-16 04:54:45 +0530

    Password change validation issue fixed

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -17,7 +17,7 @@ class PasswordController extends Controller
     {
         $validated = $request->validate([
             'current_password' => ['required', 'current_password'],
-            'password' => ['required', Password::defaults(), 'confirmed'],
+            'new_password' => ['required', Password::defaults(), 'confirmed'],
         ]);
 
         $request->user()->update([
