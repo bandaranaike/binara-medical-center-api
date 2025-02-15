e37a93bb09e9c354bbc25feb18d0ed7ca472e125
Eranda Bandaranaike <ergroove@live.com>
2025-02-16 04:58:24 +0530

    Password change validation issue fixed

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
@@ -13,7 +13,7 @@ class PasswordController extends Controller
     /**
      * Update the user's password.
      */
-    public function update(Request $request): RedirectResponse
+    public function update(Request $request): JsonResponse
     {
         $validated = $request->validate([
             'current_password' => ['required', 'current_password'],
@@ -24,6 +24,6 @@ class PasswordController extends Controller
             'password' => Hash::make($validated['new_password']),
         ]);
 
-        return back();
+        return new JsonResponse("Password updated successfully.", 201);
     }
 }
